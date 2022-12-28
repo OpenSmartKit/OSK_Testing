f5c1eab222a6a695eb2e9725a6e196a51b729dcc
Bogdan Symchych <bsymchych@advancedmd.com>
2022-12-28 14:48:40 +0200

    Use updated config

--- src/settings/ModuleSettings.h
+++ src/settings/ModuleSettings.h
@@ -21,17 +21,7 @@ public:
     virtual void setWifiName(char *value);
     virtual char *getWifiPassword();
     virtual void setWifiPassword(char *value);
-
-    virtual uint8_t getState();
-    virtual uint16_t getCurrentStep();
-    virtual uint8_t getCountOfCases();
-    virtual ThermCases *getCases();
-
-    virtual void setState(uint8_t value);
-    virtual void setCurrentStep(uint16_t value);
-    virtual void setCountOfCases(uint8_t value);
-    virtual void setCases(ThermCases *value, uint8_t size);
-
+    
 private:
     Settings _settings;
 
